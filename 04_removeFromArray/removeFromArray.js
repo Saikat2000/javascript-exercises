f724d411dac29b7fff3db90ef1f8d979df0e915a
Saikat De <81856219+Saikat2000@users.noreply.github.com>
2023-05-04 19:32:21 +0530

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...args ) {
+    for (let i=1; i<arguments.length; i++){
+        if(array.indexOf(arguments[i])>=0){
+            
+            array.splice(array.indexOf(arguments[i]),1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
