66e8135e0d433742ddf6481284762b675b9516fb
Saikat De <81856219+Saikat2000@users.noreply.github.com>
2023-05-04 21:08:29 +0530

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    if(typeof(num1)==='number' && typeof(num2)==='number'){
+        
+        if(num1>=0 && num2>=0){
+            let n=Math.abs(num2-num1)+1;
+            let sum=((num1+num2)*n)/2;
+            return sum;
+        }
+        else return "ERROR";
+    }
+    else return "ERROR";
+    
 };
 
 // Do not edit below this line
