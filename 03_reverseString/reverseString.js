5052de9c3ed9e3fd4fe9838d3deaf61dce8d59b6
Saikat De <81856219+Saikat2000@users.noreply.github.com>
2023-05-04 09:06:46 +0530

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let size=string.length;
+    let reverseString='';
+    for(let i=size-1; i>=0; i--){
+        reverseString += string.charAt(i);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
